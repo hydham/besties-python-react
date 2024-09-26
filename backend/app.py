15d4da0a6aa2154a5e89cc7e322514800644f9e0
unknown <hydham007@gmail.com>
2024-09-26 17:05:03 +0530

    deployment fix 1

--- backend/app.py
+++ backend/app.py
@@ -1,34 +1,35 @@
+# TODO: UPDATE THIS FILE FOR DEPLOYMENT
 from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import os
 
 app = Flask(__name__)
-CORS(app)
+
+# We can comment this CORS config for the production because we are running the frontend and backend on the same server
+# CORS(app) 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['DEBUG'] = True
 
 db = SQLAlchemy(app)
 
-# save frontend static files
-dist_folder = os.path.join(os.getcwd(), "..", "frontend", "dist")
+frontend_folder = os.path.join(os.getcwd(),"..","frontend")
+dist_folder = os.path.join(frontend_folder,"dist")
 
 # Server static files from the "dist" folder under the "frontend" directory
-@app.route("/", defaults={"filename": ""})
+@app.route("/",defaults={"filename":""})
 @app.route("/<path:filename>")
 def index(filename):
-    if filename and os.path.exists(os.path.join(dist_folder, filename)):
-        return send_from_directory(dist_folder, filename)
-    return send_from_directory(dist_folder, "index.html")
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
 
-# once app and db is defined, import the routes - routes uses the models.py
+# api routes
 import routes
 
-# once the routes and models are defined, you initialize the tables and create it if not already created.
 with app.app_context():
-    db.create_all()
+  db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
